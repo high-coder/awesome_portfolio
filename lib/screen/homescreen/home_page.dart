1f737d96bedc3e0bff80ce27b9c67e83a8f8b950
high-coder <teentechreviewer15@gmail.com>
2023-09-19 11:15:24 +0530

    step02

--- lib/screen/homescreen/home_page.dart
+++ lib/screen/homescreen/home_page.dart
@@ -1,25 +1,47 @@
 import 'package:device_frame/device_frame.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          DeviceFrame(
-            device: Devices.ios.iPhone13,
-            screen: const Center(
-              child: Text(
-                "Hello World",
-                style: TextStyle(color: Colors.white),
-              ),
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                // end: Alignment(0, -0.4),
+                colors: [Colors.blue, Colors.black45],
+              )),
             ),
-          )
-        ],
+            SvgPicture.asset(
+              "assets/images/cloudyBlue.svg",
+              // width: double.infinity,
+              height: size.height,
+              fit: BoxFit.cover,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                DeviceFrame(
+                  device: Devices.ios.iPhone13,
+                  screen: const Center(
+                    child: Text(
+                      "Hello World",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
